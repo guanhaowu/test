7b1f233654e9090a8875ba1a7d95fd967f1806d8
Guan Hao Wu <guanhao_wu@hotmail.com>
2018-04-26 21:22:41 +0200

    css changed from fixed to relative and other css changes

--- private/templates/footerTemplate.php
+++ private/templates/footerTemplate.php
@@ -1,12 +1,12 @@
                     </div><?php /* close contentTemplate.php content-container div */ ?>
                 </div><?php /* close contentTemplate.php content div */ ?>
-            </div><?php /* close Wrapper div */ ?>
-            <?php /* Footer area */ ?>
+			<?php /* Footer area */ ?>
             <div id="footer-container">
                 <div id="footer">
                     <div>Copyright &#169; <?php echo date('Y'); ?> Object Oriented Programming by Guan Hao Wu</div>
                 </div>
-            </div>            
-        </div><?php /* close headerTemplate.php page div */ ?>
+            </div> 
+            </div><?php /* close Wrapper div */ ?>      
+        </div><?php /* close headerTemplate.php page div */ ?>     
     </body><?php /* close headerTemplate.php body */ ?>
 </html><?php /* close headerTemplate.php html */ ?>
